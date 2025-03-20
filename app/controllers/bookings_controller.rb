bf04b0f83ffc8f940670a8db12aa702e5594b530
Lotfi Bensmail <bensmainelotfi@hotmail.fr>
2025-03-20 16:36:56 +0100

    Réalisation de la fonctionalité booking+migration

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,36 +1,47 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorize_booking, only: [:create, :edit, :destroy]
+  # before_action :authorize_booking, only: [:create, :edit, :destroy]
 
   def index
     @bookings = current_user.bookings.includes(:professional)
   end
 
-  # def show
-  #   # créneaux idéaux sans prise en compte des rdv déjà pris
-  # @slot = [
-  #   ["09h00-10h00", Time.new(2022, 3, 26, 9, 0, 0, '+04:00')],
-  #   ["10h00-11h00", Time.new(2022, 3, 26, 10, 0, 0, '+04:00')],
-  #   ["11h00-12h00", Time.new(2022, 3, 26, 11, 0, 0, '+04:00')],
-  #   ["14h00-15h00", Time.new(2022, 3, 26, 14, 0, 0, '+04:00')],
-  #   ["15h00-16h00", Time.new(2022, 3, 26, 15, 0, 0, '+04:00')],
-  #   ["16h00-17h00", Time.new(2022, 3, 26, 15, 0, 0, '+04:00')],
-  #   ["17h00-18h00", Time.new(2022, 3, 26, 15, 0, 0, '+04:00')]
-  # ]
-  # end
+  def show
+    # créneaux idéaux sans prise en compte des rdv déjà pris
+  @slot = [
+    ["09h00-10h00", Time.new(2022, 3, 26, 9, 0, 0, '+04:00')],
+    ["10h00-11h00", Time.new(2022, 3, 26, 10, 0, 0, '+04:00')],
+    ["11h00-12h00", Time.new(2022, 3, 26, 11, 0, 0, '+04:00')],
+    ["14h00-15h00", Time.new(2022, 3, 26, 14, 0, 0, '+04:00')],
+    ["15h00-16h00", Time.new(2022, 3, 26, 15, 0, 0, '+04:00')],
+    ["16h00-17h00", Time.new(2022, 3, 26, 15, 0, 0, '+04:00')],
+    ["17h00-18h00", Time.new(2022, 3, 26, 15, 0, 0, '+04:00')]
+  ]
+  end
+
   def new
     @booking = Booking.new
     authorize @booking
     @professional_service = ProfessionalService.find(params[:professional_service_id])
     @professional = @professional_service.professional
     @service = @professional_service.service
+    start_time = Time.new(2025, 3, 20, 8, 0, 0) # Début à 08:00
+    end_time = Time.new(2025, 3, 20, 18, 0, 0) # Fin à 18:00
+
+    @slots = (start_time.to_i..end_time.to_i).step(3600).map do |t|
+      [Time.at(t).strftime("%H:%M"), Time.at(t)]
+    end
+
+    puts @slots
   end
 
   def create
-    @booking = @service.Booking.build(booking_params)
+    @booking = Booking.new(booking_params)
+    @professional_service = ProfessionalService.find(params[:professional_service_id])
     @booking.user = current_user
-    @booking.professional = @professional
-    if @booking.save
+    @booking.professional_service = @professional_service
+    authorize @booking
+    if @booking.save!
       redirect_to bookings_path, notice: "Réservation validée"
     else
       render :new, status: :unprocessable_entity
--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -4,9 +4,14 @@ class BookingPolicy < ApplicationPolicy
   # In most cases the behavior will be identical, but if updating existing
   # code, beware of possible changes to the ancestors:
   # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
-def new?
-true
-end
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+  
   class Scope < ApplicationPolicy::Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
