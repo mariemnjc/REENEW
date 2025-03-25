cdb525eadc79d7d6e78d2272de5bc3e6f6276ff7
Marie Menjucq <mariemnjc@gmail.com>
2025-03-25 14:36:23 +0100

    Add mapbox

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem "pg", "~> 1.1"
 gem "puma", ">= 5.0"
 
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
-gem "importmap-rails"
+gem "importmap-rails", "~> 1.2.3"
 
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "turbo-rails"
@@ -57,6 +57,10 @@ gem "pg_search"
 
 # Cloudinary
 gem "cloudinary"
+
+# Geocoder
+gem "geocoder"
+
 
 group :development, :test do
   gem "dotenv-rails"
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js", preload: true
 pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.1.2/dist/mapbox-gl.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.1.0/nodelibs/browser/process-production.js"
