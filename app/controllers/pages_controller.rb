f4c07a5936dfd40c71a9862e49040de87e2abd1e
Valentin Malemanche <vmalemanche@gmail.com>
2025-03-26 15:51:25 +0100

    nouvelle home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,10 +10,14 @@ class PagesController < ApplicationController
   def home
     @salons = Salon.all
     @services = Service.all
+
     if params[:query].present?
       @services = @services.where("title ILIKE ?", "%#{params[:query]}%")
       @salons = @salons.where("title ILIKE ?", "%#{params[:query]}%")
       redirect_to search_path
     end
+
+    # 👇 Simule des salons recommandés pour la home
+    @recommended_salons = Salon.order(created_at: :desc).limit(3)
   end
 end
